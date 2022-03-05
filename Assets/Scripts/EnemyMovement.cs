4f8b6c0cbae88cdf3391143f807b3cd191c4c1bb
Xavier <xavier.veyrier@gmail.com>
2022-03-05 13:18:41 +0100

    Enemy added + dying anim
    working on sprite color invoke method

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float enemyHorizontalSpeed = 1f;
+    Rigidbody2D rbEnemy;
+    BoxCollider2D enemyBoxCollider;
+    
+    void Awake()
+    {
+        enemyBoxCollider = GetComponent<BoxCollider2D>();
+        rbEnemy = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        rbEnemy.velocity = new Vector2(enemyHorizontalSpeed, 0f);        
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        enemyHorizontalSpeed = -enemyHorizontalSpeed;
+        FlipEnemySprite();
+    }
+
+    void FlipEnemySprite()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rbEnemy.velocity.x)), 1f);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,49 +8,63 @@ public class PlayerMovement : MonoBehaviour
     [Header("Player tuning")]
     [SerializeField] float horizontalSpeed = 7.5f;
     [SerializeField] float verticalSpeed = 5f;
-    [SerializeField] float jumpSpeed = 10f;
+    [SerializeField] float jumpSpeed = 12f;
     [SerializeField] float climbingHorizontalSpeed = 2f;
+    [SerializeField] int playerHealth = 2;
 
     Vector2 moveInput;
     Rigidbody2D rbPlayer;
     Animator playerAnimator;
     CapsuleCollider2D playerCapsuleCollider;
+    BoxCollider2D playerFeetBoxCollider;
+    SpriteRenderer playerSpriteRenderer;
+    Color defaultPlayerSpriteColor = new Color(255f, 255f, 255f, 255f);
 
     bool playerHasHorizontalSpeed = false;
     bool playerHasVerticalSpeed = false;
     bool isAttachedToClimbing = false;
+    bool isAlive = true;
+    int currentHealth;
     float defaultPlayerGravity;
     float defaultHorizontalSpeed;
     float defaultClimbAnimMultiplier;
+    
 
     void Awake()
     {
         playerAnimator = GetComponent<Animator>();
         rbPlayer = GetComponent<Rigidbody2D>();
         playerCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        playerFeetBoxCollider = GetComponent<BoxCollider2D>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Start() 
     {
         defaultPlayerGravity = rbPlayer.gravityScale; 
         defaultHorizontalSpeed = horizontalSpeed;
+        currentHealth = playerHealth;
         defaultClimbAnimMultiplier = playerAnimator.GetFloat("climbingAnimSpeed");
     }
     
     void Update()
     {
+        if (!isAlive) { return; } 
+
         Run();
         Climb();
+        OnHit();
     }
 
     void OnMove(InputValue value)
     {
+        if (!isAlive) { return; } 
         moveInput = value.Get<Vector2>();
     }
 
     void OnJump(InputValue value)
     {
-        if (!playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+        if (!playerFeetBoxCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) || !isAlive) return;
 
         if(value.isPressed)
         {
@@ -78,12 +92,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Climb()
     {        
-        if (!playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        if (!playerFeetBoxCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
             ResetClimbingState(); 
             return;
         }
-        else if (playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")) && moveInput.y > Mathf.Epsilon)
+        else if (playerFeetBoxCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")) && moveInput.y > Mathf.Epsilon)
         {
             isAttachedToClimbing = true;
         }
@@ -116,5 +130,34 @@ public class PlayerMovement : MonoBehaviour
         rbPlayer.gravityScale = defaultPlayerGravity;
         playerAnimator.SetBool("isClimbing", false);
         playerAnimator.SetFloat("climbingAnimSpeed", defaultClimbAnimMultiplier);
+    }
+
+    void OnHit()
+    {        
+        Vector2 hitVelocity = new Vector2(-(transform.localScale.x) * 20f, 10f);
+        if (playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")) && currentHealth > 0)
+        {
+            rbPlayer.velocity = hitVelocity;           
+            playerSpriteRenderer.material.color = Color.red;
+            Invoke("SubtractPlayerHealth", 0.1f);
+        }
+        else if (currentHealth == 0)
+        {
+            isAlive = false;
+            playerAnimator.SetTrigger("Dying");
+        }
+        
+        Invoke("RestorePlayerSpriteColor", 0.1f); // TODO: Invoke not working for restoring sprite color!
+    }
+
+    void SubtractPlayerHealth()
+    {
+        currentHealth--;
+    }
+
+    void RestorePlayerSpriteColor()
+    {
+        playerSpriteRenderer.color = new Color(255f, 255f, 255f, 255f);
+        Debug.Log(defaultPlayerSpriteColor);
     }
 }
